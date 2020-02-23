ecd927ffb9aea8af2b31cf33a4d60fde6a3b3d8d
Volodymyr Dytynyuk <vdytynyuk@amcbridge.com>
2020-02-23 22:30:51 +0200

    Solution (just added default redirect)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import ItemPage from './components/ItemPage/ItemPage';
 import Home from './components/Home';
@@ -9,6 +9,7 @@ ReactDOM.render(
     <Router>
         <Navbar />
         <Switch>
+            <Redirect exact from="/" to="items" />
             <Route exact path="/items" component={Home} />
             <Route exact path="/items/:id" component={ItemPage} />
         </Switch>
